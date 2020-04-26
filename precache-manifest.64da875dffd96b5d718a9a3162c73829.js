245608ba30dba9d29b917abe57726427db936fda
samarmahmoud <samarmahmoud1516@gmail.com>
2020-04-26 18:06:31 +0200

    Updates

---
+++ precache-manifest.64da875dffd96b5d718a9a3162c73829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36196f11ae869211a3db",
+    "url": "/react-to-do/static/css/main.237fea48.chunk.css"
+  },
+  {
+    "revision": "36196f11ae869211a3db",
+    "url": "/react-to-do/static/js/main.fbfff8db.chunk.js"
+  },
+  {
+    "revision": "26b717aa96a48dc90608",
+    "url": "/react-to-do/static/js/runtime~main.8def430e.js"
+  },
+  {
+    "revision": "3081340526ed7526a06e",
+    "url": "/react-to-do/static/js/2.c92c8dbf.chunk.js"
+  },
+  {
+    "revision": "1b71e7154e5d7d8c2b0aee5176e29d95",
+    "url": "/react-to-do/index.html"
+  }
+];
